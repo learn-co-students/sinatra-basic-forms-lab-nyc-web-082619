953530893f091dcec82b4dce6d14153465538691
Tania Manco <belalov.tania16@gmail.com>
2019-09-18 13:42:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end 
+
+    get '/new' do
+        erb :create_puppy
+    end 
+
+    post '/puppy' do
+        # binding.pry
+        @puppy = Puppy.new(params[:name],params[:breed],params[:months_old])
+        redirect :puppy
+    end
+
+    get '/puppy' do
+        erb :display_puppy
+    end
 end
